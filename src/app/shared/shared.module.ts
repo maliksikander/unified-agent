6da8a6305889487e1fdce12b31746be86e66fcdb
farhan.maqbool <farhan.maqbool@expertflow.com>
2020-10-27 19:06:58 +0500

    import bootstrap and angular modules/ add Login page and change classes materialize.css to bootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,41 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  MatAutocompleteModule,
+  MatButtonModule, MatCardModule,
+  MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatDrawer, MatExpansionModule, MatFormFieldModule,
+  MatIconModule, MatInputModule,
+  MatListModule, MatMenuModule, MatNativeDateModule, MatProgressBarModule, MatProgressSpinnerModule,
+  MatRadioModule, MatSelectModule,
+  MatSidenavModule, MatSlideToggleModule, MatSnackBarModule, MatTableModule, MatTabsModule,
+  MatToolbarModule, MatTooltipModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 
 @NgModule({
   declarations: [],
   imports: [
+    MatIconModule, MatToolbarModule, MatSidenavModule,
+    BrowserAnimationsModule, MatCheckboxModule, MatButtonModule,
+    MatRadioModule, ReactiveFormsModule,
+    MatListModule, MatSnackBarModule, MatTableModule, MatProgressSpinnerModule,
+    MatProgressBarModule, MatCardModule, MatChipsModule, MatFormFieldModule,
+    MatTabsModule, MatSelectModule, MatInputModule, MatTooltipModule,
+    FormsModule, MatExpansionModule, MatMenuModule, MatSlideToggleModule,
+    MatAutocompleteModule, MatDialogModule, MatTabsModule,
+    MatDatepickerModule, MatNativeDateModule,
   ], exports: [
-    ReactiveFormsModule
+    MatIconModule, MatToolbarModule, MatSidenavModule,
+    MatDrawer, BrowserAnimationsModule, MatCheckboxModule,
+    MatButtonModule, MatRadioModule,
+    ReactiveFormsModule, MatListModule, MatSnackBarModule,
+    MatProgressSpinnerModule, MatTableModule, MatProgressBarModule,
+    MatCardModule, MatChipsModule, MatFormFieldModule, MatInputModule,
+    MatTabsModule, MatSelectModule, MatTooltipModule, FormsModule, MatExpansionModule,
+    MatMenuModule, MatSlideToggleModule, MatAutocompleteModule, MatDialogModule, MatTabsModule,
+    MatDatepickerModule, MatNativeDateModule, DragDropModule
   ]
 })
 export class SharedModule { }
