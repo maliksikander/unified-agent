cf3af9202424b25ac07f8b656f1d8981af2cdb7e
farhan.maqbool <farhan.maqbool@expertflow.com>
2021-03-04 10:20:52 +0500

    add Agent Announcement view

--- src/app/announcement/agent-announcements/agent-announcements.component.ts
+++ src/app/announcement/agent-announcements/agent-announcements.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-agent-announcements',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agent-announcements.component.scss']
 })
 export class AgentAnnouncementsComponent implements OnInit {
+  announcements = [
+    {
+      message: 'The office will remain closed on public holiday on December 25th, 2019. Teams with working shift will get a day off in the coming week.',
+      startTime: 'January 16, 2020 - 09:35AM'
+    }, {
+      message: 'The office will remain closed on public holiday on December 25th, 2019. Teams with working shift will get a day off in the coming week. The office will remain closed on public holiday on December 25th, 2019. Teams with working shift will get a day off in the coming week. ',
+      startTime: 'January 16, 2020 - 09:35AM'
+    }, {
+      message: 'The office will remain closed on public holiday on December 25th, 2019. Teams with working shift will get a day off in the coming week.',
+      startTime: 'January 16, 2020 - 09:35AM'
+    }, {
+      message: 'The office will remain closed on public holiday on December 25th, 2019. Teams with working shift will get a day off in the coming week.',
+      startTime: 'January 16, 2020 - 09:35AM'
+    }, {
+      message: 'The office will remain closed on public holiday on December 25th, 2019. Teams with working shift will get a day off in the coming week.',
+      startTime: 'January 16, 2020 - 09:35AM'
+    },
 
-  constructor() { }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+  onUnreadClick(announcement) {
+    announcement.isRead = true;
   }
 
 }
