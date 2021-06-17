5ca2ac6071185e5bddfa6a23d192991e9eb9e595
raza.ellahi <raza.ellahi@expertflow.com>
2021-06-17 22:37:50 +0500

    initial push

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -11,18 +11,84 @@ import { MatSidenav } from "@angular/material";
 })
 export class AppHeaderComponent implements OnInit {
   agent = {
-    state: "blbla",
+    state: "ready",
     name: "Bryan Miller",
     extension: 1126
   };
   isConnected = true;
+  changeLanguageCode = 'en';
   languageFlag = "en.png";
   languageName = "English";
   languages = [
     { code: "en", name: "English", flag: "en.png" },
     { code: "fr", name: "French", flag: "fr.png" }
   ];
+  logoutReasons = [
+    'Done for the day',
+    'Vacation',
+    'Work Travel',
+    'Attend a Meeting',
+    'System Issue',
+    'Training',
+    'Not Feeling Well',
+    'Shift Ended',
+    'Done for the day',
+    'Vacation'
+  ];
 
+
+  llp = {
+    "agent": {
+      "participantType": "CCUser",
+      "id": "8d42617c-0603-4fbe-9863-2507c0fff9fd",
+      "keycloakUser": {
+        "id": "8d42617c-0603-4fbe-9863-2507c0fff9fd",
+        "firstName": "Nabeel",
+        "lastName": "Ahmad",
+        "username": "nabeel",
+        "permittedResources": {
+          "Resources": [
+            {
+              "rsid": "e6c56b53-e53e-41b1-8d85-1101172f3029",
+              "rsname": "Default Resource"
+            }
+          ]
+        },
+        "roles": [
+          "agent",
+          "customer-manager",
+          "offline_access",
+          "uma_authorization"
+        ],
+        "realm": "university"
+      },
+      "associatedRoutingAttributes": [
+
+      ]
+    },
+    "state": {
+      "name": "READY",
+      "reasonCode": "NONE"
+    },
+    "stateChangeTime": 1623892004029,
+    "agentMrdStates": [
+      {
+        "mrd": {
+          "id": "124564313",
+          "name": "CHAT"
+        },
+        "state": {
+          "name": "LOGOUT",
+          "reasonCode": "NONE"
+        },
+        "stateChangeTime": 1623892003737
+      }
+    ]
+  }
+
+
+  changeLanguage = false;
+  logoutReasonList = false;
   startTime: Date;
   stopTime: Date;
   active: boolean = false;
@@ -39,7 +105,7 @@ export class AppHeaderComponent implements OnInit {
     }
   }
 
-  constructor(public _cacheService: cacheService, private _socketService: socketService, private _sharedService: sharedService) {}
+  constructor(public _cacheService: cacheService, private _socketService: socketService, private _sharedService: sharedService) { }
 
   ngOnInit() {
     this._sharedService.serviceCurrentMessage.subscribe((e) => {
@@ -58,11 +124,11 @@ export class AppHeaderComponent implements OnInit {
 
   changeState(state) {
     if (state == 0) {
-      this._socketService.emit("changeAgentState", { agentId: this._cacheService.agent.id, state: "NOT_READY" });
+      this._socketService.emit("changeAgentState", { agentId: this._cacheService.agent.id, action: "agentState", state: { name: "NOT_READY", reasonCode: "" } });
     }
 
     if (state == 1) {
-      this._socketService.emit("changeAgentState", { agentId: this._cacheService.agent.id, state: "READY" });
+      this._socketService.emit("changeAgentState", { agentId: this._cacheService.agent.id, action: "agentState", state: { name: "READY", reasonCode: "" } });
     }
   }
 
@@ -71,7 +137,23 @@ export class AppHeaderComponent implements OnInit {
     if (selectedLanguage !== undefined) {
       this.languageName = selectedLanguage.name;
       this.languageFlag = selectedLanguage.flag;
+      this.changeLanguageCode = lang;
     }
   }
-  close() {}
+
+  updateLanguage() {
+    this.changeLanguage = true;
+  }
+  logout() {
+    this.logoutReasonList = true;
+
+  }
+
+  changeMRD(event, agentMrdState) {
+
+    this._socketService.emit("changeAgentState", { agentId: this._cacheService.agent.id, action: "agentMRDState", state: event.checked == true ? 'READY' : 'NOT_READY', mrdId: agentMrdState.mrd.id });
+
+  }
+
+  close() { }
 }
