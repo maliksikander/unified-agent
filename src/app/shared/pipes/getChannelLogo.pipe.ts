2927848dde002757f609133e7ba88343094c6de8
farhan.maqbool <farhan.maqbool@expertflow.com>
2021-11-04 18:52:22 +0500

    Update UI Components add flags icons

--- src/app/shared/pipes/getChannelLogo.pipe.ts
+++ src/app/shared/pipes/getChannelLogo.pipe.ts
@@ -1,16 +1,18 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import { httpService } from "src/app/services/http.service";
 import { sharedService } from "src/app/services/shared.service";
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Pipe({ name: "channelLogo" })
 export class channelLogoPipe implements PipeTransform {
-  constructor(private _sharedService: sharedService, private _httpService: httpService) {}
+  constructor(private domSanitizer: DomSanitizer, private _sharedService: sharedService, private _httpService: httpService) {}
   transform(filename: any): any {
     return new Promise((resolve) => {
       if (filename) {
         // check if file is exists in memory
         if (this._sharedService.channelLogoMapper.get(filename)) {
-          resolve(this._sharedService.channelLogoMapper.get(filename));
+          resolve(this.domSanitizer.bypassSecurityTrustUrl(this._sharedService.channelLogoMapper.get(filename).toString()));
         } else {
           // if not found in memory then get it from file server
           this._httpService.getChannelLogo(filename).subscribe(
@@ -20,11 +22,11 @@ export class channelLogoPipe implements PipeTransform {
               reader.onloadend = () => {
                 // saving that file for the next time
                 this._sharedService.channelLogoMapper.set(filename, reader.result);
-                resolve(reader.result);
+                resolve(this.domSanitizer.bypassSecurityTrustUrl((reader.result).toString()));
               };
             },
             (err) => {
-              resolve(null);
+              resolve('assets/flags/generic.png');
             }
           );
         }
