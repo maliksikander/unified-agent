5bc857307a9aba80923895db428f0abe47a87efc
raza.ellahi <raza.ellahi@expertflow.com>
2022-03-06 21:22:00 +0500

    handle sw file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,7 +41,7 @@ let pwaServiceWorkerDev = [
   ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production, registrationStrategy: 'registerImmediately' })
 
 ];
-console.log("V.1");
+console.log("V.1.0");
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   console.log("device is mobile");
